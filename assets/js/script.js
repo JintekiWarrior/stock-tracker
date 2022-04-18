0c91e05d2ec7590a8a746eb168ab036c49bee75a
JintekiWarrior <jintekiwarrior@gmail.com>
2022-04-17 22:16:45 -0400

    Add stock symbol submit form

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,32 @@
+const stockSymbolForm = document.querySelector("#stock-symbol-form")
+
+const stockSymbolButton = document.querySelector("#stock-symbol-button")
+
+// const submitStockSymbol = (button, value) => {
+//   let stockSymbol
+//   button.addEventListener("click", (e) => {
+//     e.preventDefault()
+//     console.log(value)
+//   })
+//   return stockSymbol 
+// }
+let stockSymbol
+
+const submitStockFormHandler = e => {
+  e.preventDefault()
+  const stockSymbolInput = document.querySelector("input[name='stock-symbol-input']").value
+  stockSymbol = stockSymbolInput
+}
+
+stockSymbolForm.addEventListener("submit", submitStockFormHandler)
+
+stockSymbolButton.addEventListener("click", e => {
+  e.preventDefault()
+  console.log(stockSymbolInput)
+})
+
+submitStockSymbol(stockSymbolButton, stockSymbolInput)
+
 // This API returns intraday time series (timestamp, open, high, low, close, volume) of the equity specified.
 const url = `https://alpha-vantage.p.rapidapi.com/query?interval=5min&function=TIME_SERIES_INTRADAY&symbol=${stockSymbol}&datatype=json&output_size=compact`
 
