755cc721f2963b5d89ab8ab14217c8c028680bcb
JintekiWarrior <jintekiwarrior@gmail.com>
2022-04-17 23:47:27 -0400

    Got chart.js to work with the stock data - Still need to test if it is working properly - Noticed bug while zooming into chart

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,6 +2,22 @@ const stockSymbolForm = document.querySelector("#stock-symbol-form")
 
 let stockSymbol
 
+const separateKeyAndValue = (obj) => {
+  const result = {
+    keys: [],
+    values: []
+  }
+
+  for (let key in obj) {
+    let value = obj[key]
+
+    result.keys.push(key)
+    result.values.push(value)
+  }
+
+  return result
+}
+
 const submitStockFormHandler = e => {
   e.preventDefault()
   const stockSymbolInput = document.querySelector("input[name='stock-symbol-input']").value
@@ -15,12 +31,57 @@ const submitStockFormHandler = e => {
       'X-RapidAPI-Host': 'alpha-vantage.p.rapidapi.com',
       'X-RapidAPI-Key': 'b999665da7msh0b6f060f93e1a73p1a683bjsn682b079d95ac'
     }
-  } 
+  }
+
+  let stockData
 
   const getFinData = (url, options) => {
     fetch(url, options)
     .then(response => response.json())
-    .then(response => console.log(response))
+    .then(response => {
+      stockData = separateKeyAndValue(response["Time Series (5min)"])
+      return stockData
+    })
+    .then(stockData => {
+      const ctx = document.querySelector('#myChart')
+
+      let stockOpen = []
+
+      for (let i = 0; i < stockData.values.length; i++) {
+        stockOpen.push(stockData.values[i]["1. open"])
+      }
+
+      console.log("keys", stockData.keys)
+
+      const myChart = new Chart(ctx, {
+        // tells what type of chart
+        type: 'line',
+        // data going into the chart
+        data: {
+          labels: stockData.keys.reverse(),
+          datasets: [
+            {
+              // represents this dataset
+              label: stockSymbol,
+              data: stockOpen
+            }
+          ]
+        }
+      })
+
+      console.log("values", stockOpen)
+      
+      const data = {
+        labels: stockOpen,
+        datasets: [{
+          label: 'My First Dataset',
+          data: stockOpen.reverse(),
+          fill: false,
+          borderColor: 'rgb(75, 192, 192)',
+          tension: 0.1
+        }]
+      };      
+    })
     .catch(err => console.error(err));
   }
 
@@ -28,4 +89,7 @@ const submitStockFormHandler = e => {
 }
 
 stockSymbolForm.addEventListener("submit", submitStockFormHandler)
+
+
+
 
